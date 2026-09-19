5a2f90d7483b31820a2e4f4d8918845182e50929
Julian Starks <julianstarks218@gmail.com>
2026-09-19 02:02:16 +0000

    compiled horseRace

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # CS121_project_4
+## "horseRace.cpp" algorithm
+```
+functions take name of horse (number) and position of horses
+Track postion of horses inside array (length of 15)
+Create race track using strings
+Take user input to coin flip determing whether a horse goes forward (0/1)
+Keep looping until horse hits end of track
+Print name of horse that wins
+```
+## "advance" algorithm
+```
+use random number generation to coin flip
+if 0 stay, if 1 move
+```
+## "printLane" algorithm
+```
+loop through horse array 
+if horse array doesnt match with position of horse print track
+```
+## "isWinner" algorithm
+```
+boolean function
+check position of horse array with length of track
+if true print name of horse has won
+```
---
+++ horseRace.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <random>
+
+std::random_device rd;
+std::uniform_int_distribution<int> dist(0, 1);
+
+const int TRACK_LENGTH = 15;
+const int MAX_HORSES = 5;
+
+void advance(int horseNum, int* horses);
+void printLane(int horseNum, int* horses);
+bool isWinner(int horseNum, int* horses);
+
+int main() {
+	int position[MAX_HORSES] = {0};
+	bool keepGoing = true;
+
+	while (keepGoing) {
+		std::cout << "Press enter for another turn";
+		std::cin.get();
+
+		for (int i = 0; i < MAX_HORSES; i++) {
+			advance(i, position);
+			printLane(i, position);
+
+			if (isWinner(i, position)) {
+				std::cout << "Horse " << i << " WINS!!!" << std::endl;
+				keepGoing = false;
+				break;
+			}
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
+
+void advance(int horseNum, int* horses) {
+	int coin = dist(rd);
+	horses[horseNum] += coin;
+}
+
+void printLane(int horseNum, int* horses){
+	for (int i = 0; i < TRACK_LENGTH; i++) {
+		if (i == horses[horseNum]) {
+			std::cout << horseNum;
+		} else {
+			std::cout << ".";
+		}
+	}
+	std::cout << std::endl;
+}
+
+bool isWinner(int horseNum, int* horses){
+	return horses[horseNum] >= (TRACK_LENGTH);
+}
